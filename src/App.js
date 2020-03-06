c3c1fada683db19b71e51a06017332ca0af21054
mestrpra <mestry.prashant10@gmail.com>
2020-03-06 22:25:57 +0530

    new folder react automatoc first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is header of react automatic      
+      </header>
+    </div>
+  );
+}
+
+export default App;
